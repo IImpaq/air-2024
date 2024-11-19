d1bab87c893b5473e12cc69611e737413099caf7
Marcus Gugacs <m.gugacs@student.tugraz.at>
2024-11-19 13:34:04 +0100

    frontend: added placeholder data

---
+++ frontend/src/lib/placeholder-data.ts
@@ -0,0 +1,30 @@
+export const placeholderMovies = [
+  {
+    id: '1',
+    title: 'Inception',
+    year: 2010,
+    poster: 'https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg',
+    rating: 8.1
+  },
+  {
+    id: '2',
+    title: 'Deadpool & Wolverine',
+    year: 2024,
+    poster: 'https://www.themoviedb.org/t/p/w500/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg',
+    rating: 8.2
+  },
+  {
+    id: '3',
+    title: 'Everything Everywhere All at Once',
+    year: 2022,
+    poster: 'https://image.tmdb.org/t/p/w500/w3LxiVYdWWRvEVdn5RYq6jIqkb1.jpg',
+    rating: 8.3
+  },
+  {
+    id: '4',
+    title: 'Parasite',
+    year: 2019,
+    poster: 'https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg',
+    rating: 8.4
+  }
+];
